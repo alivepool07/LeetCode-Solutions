6887301cb299a377638ed2794b0d0220c495a833
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2024-07-18 23:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (91.15%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,19 +1,25 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    public int search(int[] nums, int tar) {
         
-        int n = nums.length;
-        int low = 0;
-        int high = n-1;
-       
-    while(low <= high) {
-        
-        int mid =  low + ((high-low)/2) ;
 
-        if(nums[mid] == target) return mid;
-        else if(nums[mid] > target) high = mid -1;
-        else low = mid + 1;
-       
-    }      
-return -1;
-}
+        //recursive approach 
+
+        int n = nums.length;
+        return bs(nums , 0 , n-1 , tar);
+
+    }
+    public static int bs(int[] arr , int low , int high, int tar) {
+
+        //base case
+        if(low > high ) return -1;
+
+        int mid = low + ((high-low)/2);
+        if(arr[mid] == tar) return mid;
+        else if (arr[mid] > tar) {
+          return  bs(arr , low, mid-1 , tar);
+        }
+        else {
+            return bs(arr , mid+1 , high ,tar);
+        }
+    }
 }
