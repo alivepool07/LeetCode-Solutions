63e9afdb228394c889b30138a0c469409ad38751
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2024-07-30 23:43:41 +0530

    Time: 5 ms (92.94%) | Memory: 45.5 MB (6.35%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,17 +1,41 @@
 
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+//         int n = s.length();
+//         for (int i = 0; i < n; i++) {
+//             char c = s.charAt(i);
+//             count.put(c, count.getOrDefault(c, 0) + 1);
+//         }
+      
+// for (int i = 0; i < n; i++) {
+//             if (count.get(s.charAt(i)) == 1) 
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
         int n = s.length();
+        int[] count = new int[26]; // Assuming lowercase English letters
+
+        // Build the frequency map
         for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
-            count.put(c, count.getOrDefault(c, 0) + 1);
+            count[c - 'a']++;
         }
-      
-for (int i = 0; i < n; i++) {
-            if (count.get(s.charAt(i)) == 1) 
+
+        // Find the first unique character
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (count[c - 'a'] == 1) {
                 return i;
+            }
         }
-        return -1;
+
+        return -1; // No unique character found
     }
 }
+
