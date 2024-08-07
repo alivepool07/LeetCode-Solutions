cd15f92854f5a929dfc74f1d834d185b7027d0bb
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2024-08-08 00:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (62.98%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,27 +1,30 @@
 class Solution {
     public double myPow(double x, int n) {
         
-
-        //Iterative approach hai :)
-
-        if(n == 0) return 1.0;
-        long nTemp = n;
-        if (n < 0) {
-            x = 1 / x; 
-            nTemp = -nTemp;
-        }
-
         double ans = 1.0;
-        while (nTemp > 0) {
-            if (nTemp % 2 == 0) {
-                x *= x;
-                nTemp /= 2;
-            } else {
-                ans *= x;
-                nTemp--;
-            }
-        }
+        int tempN = n;
 
-        return ans;
+        if(n < 0) {
+            tempN = -tempN;
+            return 1/powRecursive(x , tempN);
+        }
+        return powRecursive(x ,n);
+    }
+
+    public double powRecursive(double x , int n) {
+
+        //base conditions
+
+        if(n == 0) return 1;
+        if(n == 1) return x;
+
+        double halfPow =  powRecursive(x , n/2);
+
+        if(n % 2 == 0) return halfPow * halfPow;
+
+        else 
+        {
+            return  halfPow * halfPow * x;
+            }
     }
 }
