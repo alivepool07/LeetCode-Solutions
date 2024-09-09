6493b6a017272f2d4cc152ef09c850bb991e38ed
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2024-09-09 22:49:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (90.09%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,22 +1,36 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        int [] dp = new int[n];
+        // int n = nums.length;
+        // int [] dp = new int[n];
 
-        if(nums == null || n == 0) return 0;
-        if(n == 1) return nums[0];
+        // if(nums == null || n == 0) return 0;
+        // if(n == 1) return nums[0];
 
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0] , nums[1]);
+        // dp[0] = nums[0];
+        // dp[1] = Math.max(nums[0] , nums[1]);
 
-        for(int i = 2 ; i < n ; i++) {
-            dp[i] = Math.max(dp[i-1] , nums[i] + dp[i-2]); 
-            // dp[i-1] ka mtlb hai ki us ghar ko nhi loot rhe
-            // nums[i] ka mtlb hai loot rhe , aur agar loot rhe hai to add kro peechle loote hue maal me 
-            // aur wo peechla hai dp[i-2] me coz second last ghar loote the. AB INHI KA MAX TO CHAHIYE :)
+        // for(int i = 2 ; i < n ; i++) {
+        //     dp[i] = Math.max(dp[i-1] , nums[i] + dp[i-2]); 
+        //     // dp[i-1] ka mtlb hai ki us ghar ko nhi loot rhe
+        //     // nums[i] ka mtlb hai loot rhe , aur agar loot rhe hai to add kro peechle loote hue maal me 
+        //     // aur wo peechla hai dp[i-2] me coz second last ghar loote the. AB INHI KA MAX TO CHAHIYE :)
+        // }
+        // return dp[n-1];
+
+// SPACE OPTIMIZATION  
+
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+
+        int prev = 0;
+        int nextPrev = 0;
+
+        for(int num : nums) {
+            int temp = prev;
+            prev = Math.max(prev , num + nextPrev);
+            nextPrev = temp;
+
         }
-        return dp[n-1];
-
-
+        return prev;
     }
 }
