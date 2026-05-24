e47843a73daa13d916d07c0a4f41bb7a45bcd7c6
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2026-05-24 23:40:24 +0530

    [242] [Time Beats: 62.54%] [Memory Beats: 74.51%] - LeetPush

--- Valid-Anagram.java
+++ Valid-Anagram.java
@@ -1,13 +1,16 @@
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
-3
-4         char[] sChar = s.toCharArray();
-5         char[] tChar = t.toCharArray();
-6         Arrays.sort(sChar);
-7         Arrays.sort(tChar);
-8
-9         if(Arrays.equals(sChar,tChar)) return true;
-10         else
-11         return false;
-12    }
-13}
+3        int[] arr = new int[26];
+4        if(s.length() != t.length()) return false;
+5
+6        for(int i =0; i < s.length(); i++ ){
+7            arr[s.charAt(i) - 'a']++;
+8            arr[t.charAt(i) - 'a']--;
+9
+10        }
+11        for(int v : arr){
+12            if(v != 0) return false;
+13        }
+14        return true;
+15    }
+16}
