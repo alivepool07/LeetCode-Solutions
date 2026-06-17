3ddd3dbeb7fbdc30f092c4dbb7d3e4a7eb2464eb
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2026-06-17 21:56:38 +0530

    [14] [Time Beats: 62.43%] [Memory Beats: 66.90%] - LeetPush

--- Longest-Common-Prefix.java
+++ Longest-Common-Prefix.java
@@ -4,10 +4,11 @@
 4        String s1 = strs[0];
 5        String s2 = strs[strs.length - 1];
 6        int i = 0;
-7        while(i < s1.length() && i < s2.length()) {
-8            if(s1.charAt(i) == s2.charAt(i)) i++;
-9            else break;
-10        }
-11        return s2.substring(0,i);
-12    }
-13}
+7
+8        while(i < s1.length() && i < s2.length()) {
+9            if(s1.charAt(i) == s2.charAt(i)) i++;
+10            else break;
+11        }
+12        return s2.substring(0,i);
+13    }
+14}
