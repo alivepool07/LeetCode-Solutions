ff21728bda33622f1f08ed6fa4091fb305524a93
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2026-06-25 19:32:19 +0530

    [796] [Time Beats: 11.16%] [Memory Beats: 12.15%] - LeetPush

---
+++ Rotate-String.java
@@ -0,0 +1,8 @@
+1class Solution {
+2    public boolean rotateString(String s, String goal) {
+3        if (s.length() != goal.length()) {
+4            return false;
+5        }
+6        return (s + s).contains(goal);
+7    }
+8}
