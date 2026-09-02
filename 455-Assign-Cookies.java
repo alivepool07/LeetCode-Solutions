f19b7d98069d7d3f32e2bfd594442d87b0a72580
Jyoti Anand <94296119+alivepool07@users.noreply.github.com>
2026-09-02 14:57:58 +0530

    [455] [Time Beats: 92.28%] [Memory Beats: 55.99%] - LeetPush

--- 455-Assign-Cookies.java
+++ 455-Assign-Cookies.java
@@ -1,19 +1,22 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        
         Arrays.sort(g);
         Arrays.sort(s);
+        int child = 0;
+        int i = 0, j = 0;
 
-        int childCount = 0;
-        int cookie = 0;
+        if(g.length == 0 || s.length == 0) return 0;
 
-        while(childCount < g.length && cookie < s.length) {
-
-            if(g[childCount] <= s[cookie]) {
-                childCount++;
+        while(i < g.length && j < s.length) {
+            if(g[i] <= s[j] ) {
+                child++;
+                i++;
+                j++;
             }
-            cookie++;
+            else {
+                j++;
+            }
         }
-        return childCount;
+        return child;
     }
 }
